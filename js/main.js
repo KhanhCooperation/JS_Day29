21d0ede9bb253cd2b0a2adae3d1544161e5f7e7b
Trần Giang Khánh <trangiangkhanh175.work@gmail.com>
2023-07-05 10:54:41 +0700

    :D

--- js/main.js
+++ js/main.js
@@ -1,4 +1,15 @@
 //option
-changeOption = () => {
-    
+changeOption = (id) => {
+    console.log("id: ", id);
+    let choice = document.getElementById("choice");
+    choice.innerText = id;
+}
+a = () => {
+    let x = document.querySelectorAll(".employ");
+    console.log("employ: ", x);
+    for (let i = 0; i < x.length; i++) {
+        console.log("x ", i, ":", x[i]);
+        x[i].classList.toggle("hidden-cus");
+        console.log("x ", i, ":", x[i]);
+    }
 }
